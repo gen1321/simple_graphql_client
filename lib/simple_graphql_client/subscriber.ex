01a5fdb55a9c9249b0afcd42d0d9075621b99253
Dennis Beatty <dennis@dcbeatty.com>
2019-04-29 23:30:46 -0600

    interpolate variables in subscription names

--- lib/simple_graphql_client/subscriber.ex
+++ lib/simple_graphql_client/subscriber.ex
@@ -4,27 +4,43 @@ defmodule SimpleGraphqlClient.Subscriber do
   """
   alias SimpleGraphqlClient.SubscriptionServer
 
+  @name_regex ~r/(?<=subscription\s).*(?=\s\{)/
+  @variable_regex ~r/\$([a-zA-Z0-9_-]*): ([a-zA-Z!_-]*)/
+
   @doc """
    Handles absinthe subscription with help of Subscritpion genserver.
   """
-  def absinthe_sub(query, variables, callback_or_dest, opts) do
+  def absinthe_sub(query, variables, callback_or_dest, _opts) do
     SubscriptionServer.subscribe(
-      get_subscription_name(query),
+      get_subscription_name(query, variables),
       callback_or_dest,
       query,
       variables
     )
   end
 
-  def get_subscription_name(query) do
-    ~r/(?<=subscription\s).*(?=\s\{)/
+  def get_subscription_name(query, variables) do
+    @name_regex
     |> Regex.run(query)
-    |> case do
-      [hd] ->
-        hd
+    |> List.first()
+    |> interpolate_variables(variables)
+  end
 
-      _ ->
-        UUID.uuid4()
-    end
+  defp interpolate_variables(name, variables) do
+    variables =
+      @variable_regex
+      |> Regex.scan(name, capture: :all_but_first)
+      |> Enum.map(fn [name, _] ->
+        value = Map.get(variables, name)
+        "$#{name}: #{inspect(value)}"
+      end)
+      |> Enum.join(", ")
+
+    name =
+      name
+      |> String.split("(")
+      |> List.first()
+
+    "#{name}(#{variables})"
   end
 end
